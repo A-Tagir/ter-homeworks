a35cec315839a0b9107291d10562901f7e8b2c5f
A-Tagir <191856809+A-Tagir@users.noreply.github.com>
2025-01-11 13:16:36 +0300

    Task 2

---
+++ 03/src/count-vm.tf
@@ -0,0 +1,31 @@
+resource "yandex_compute_instance" "web" {
+  count = 2
+  name = "web-${count.index+1}"
+  platform_id  = var.vm_web_platform_id
+
+  resources {
+    cores = var.vm_web_resources.cores
+    memory = var.vm_web_resources.memory
+    core_fraction = var.vm_web_resources.core_fraction
+  }
+  
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = var.vm_web_resources.preemptible
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat = var.vm_web_resources.nat
+    security_group_ids = [yandex_vpc_security_group.example.id]
+  }
+  metadata = {
+    serial-port-enable = var.vm_web_metadata.serial-port-enable
+#    ssh-keys           = "ubuntu:${var.metadata_resources.ssh-keys}"
+  }
+
+}
+
