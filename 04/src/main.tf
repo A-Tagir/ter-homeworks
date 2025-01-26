e81bad4ce413d138787aef821c718e7062804a0a
A-Tagir <191856809+A-Tagir@users.noreply.github.com>
2025-01-26 19:25:26 +0300

    Task 2 finished

--- 04/src/main.tf
+++ 04/src/main.tf
@@ -1,30 +1,41 @@
 #создаем облачную сеть
-resource "yandex_vpc_network" "develop" {
-  name = "develop"
-}
+#resource "yandex_vpc_network" "develop" {
+#  name = "develop"
+#}
 
 #создаем подсеть
-resource "yandex_vpc_subnet" "develop_a" {
-  name           = "develop-ru-central1-a"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = ["10.0.1.0/24"]
+#resource "yandex_vpc_subnet" "develop_a" {
+#  name           = "develop-ru-central1-a"
+#  zone           = "ru-central1-a"
+#  network_id     = yandex_vpc_network.develop.id
+#  v4_cidr_blocks = ["10.0.1.0/24"]
+#}
+
+module "vpc-develope" {
+  source         = "./vpc"
+  env_name = "develope"
+  vpc_name_a = "develop-ru-central1-a"
+  vpc_zone_a = "ru-central1-a"
+  vpc_cidr_a = ["10.0.1.0/24"]
+  vpc_name_b = "develop-ru-central1-b"
+  vpc_zone_b = "ru-central1-b"
+  vpc_cidr_b = ["10.0.2.0/24"]
 }
 
-resource "yandex_vpc_subnet" "develop_b" {
-  name           = "develop-ru-central1-b"
-  zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = ["10.0.2.0/24"]
-}
+#resource "yandex_vpc_subnet" "develop_b" {
+#  name           = "develop-ru-central1-b"
+#  zone           = "ru-central1-b"
+#  network_id     = yandex_vpc_network.develop.id
+#  v4_cidr_blocks = ["10.0.2.0/24"]
+#}
 
 
 module "accounting-vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   env_name       = "develop" 
-  network_id     = yandex_vpc_network.develop.id
+  network_id     = module.vpc-develope.yandex_network_id
   subnet_zones   = ["ru-central1-a","ru-central1-b"]
-  subnet_ids     = [yandex_vpc_subnet.develop_a.id,yandex_vpc_subnet.develop_b.id]
+  subnet_ids     = [module.vpc-develope.subnet_id_a,module.vpc-develope.subnet_id_b]
   instance_name  = "webs"
   instance_count = 2
   image_family   = "ubuntu-2004-lts"
@@ -45,9 +56,9 @@ module "accounting-vm" {
 module "marketing-vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   env_name       = "stage"
-  network_id     = yandex_vpc_network.develop.id
+  network_id     = module.vpc-develope.yandex_network_id
   subnet_zones   = ["ru-central1-a"]
-  subnet_ids     = [yandex_vpc_subnet.develop_a.id]
+  subnet_ids     = [module.vpc-develope.subnet_id_a]
   instance_name  = "web-stage"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"
